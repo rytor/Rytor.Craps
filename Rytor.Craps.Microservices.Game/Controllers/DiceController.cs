2df4d6bc537edd0becc2eeade35a2128275b6f31
Ryan Torello <32495898+rytor@users.noreply.github.com>
2020-05-17 18:07:07 -0400

    C-6 & C-13: Create Dice Library NuGet Package, Add Dice Library to Game Microservice  (#2)
    * Added Dice Library and Dummy API to Validate
    
    * Refreshed to reset gitignore

---
+++ Rytor.Craps.Microservices.Game/Controllers/DiceController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Rytor.Libraries.Dice;
+
+namespace Rytor.Craps.Microservices.Game.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DiceController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            var dice = new Dice(2, 6);
+            var result = dice.Roll();
+            return result[0].Value + "," + result[1].Value;
+        }
+    }
+}
