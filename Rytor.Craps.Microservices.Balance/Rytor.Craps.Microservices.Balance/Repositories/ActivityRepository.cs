24ee4a115d70c777733fee48a62eb8164af2413e
Ryan Torello <ryan.torello@gmail.com>
2020-07-26 15:00:43 -0400

    Finished BalanceRepository, Added ActivityRepository

---
+++ Rytor.Craps.Microservices.Balance/Rytor.Craps.Microservices.Balance/Repositories/ActivityRepository.cs
@@ -0,0 +1,166 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using Dapper;
+using Rytor.Craps.Microservices.Balance.Interfaces;
+using Models = Rytor.Craps.Microservices.Balance.Models;
+using Rytor.Craps.Microservices.Balance.Models;
+
+namespace Rytor.Craps.Microservices.Balance.Repositories
+{
+    public class ActivityRepository : IActivityRepository
+    {
+        private readonly ILogger _logger;
+        private readonly string _connectionString;
+        private readonly string _className = "ActivityRepository";
+
+        public ActivityRepository(string connectionString, ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<ActivityRepository>();
+            _logger.LogInformation("ActivityRepository initialized.");
+            _connectionString = connectionString;
+        }
+        public IEnumerable<Activity> GetActivity()
+        {
+            IEnumerable<Models.Activity> result;
+
+            string sql = $@"SELECT Id, AccountId, ActivityTypeId, Amount, CreateDate from dbo.Activity";
+
+            try
+            {
+                _logger.LogDebug($@"{_className}: Getting all Activity");
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    result = connection.Query<Models.Activity>(sql);
+                }
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($@"{_className}: Error getting Activity - {e.Message}");
+                return null;
+            }
+        }
+
+        public IEnumerable<Activity> GetActivityByAccountId(int accountId)
+        {
+            IEnumerable<Models.Activity> result;
+
+            string sql = $@"SELECT Id, AccountId, ActivityTypeId, Amount, CreateDate from dbo.Activity WHERE AccountId = @AccountId ORDER BY CreateDate ASC";
+
+            try
+            {
+                _logger.LogDebug($@"{_className}: Getting all Activity for Account {accountId}");
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    result = connection.Query<Models.Activity>(sql, new { AccountId = accountId });
+                }
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($@"{_className}: Error getting Activity for Account {accountId} - {e.Message}");
+                return null;
+            }
+        }
+
+        public Models.Activity GetActivityById(int id)
+        {
+            Models.Activity result;
+
+            string sql = $@"Id, AccountId, ActivityTypeId, Amount, CreateDate from dbo.Activity WHERE Id = @Id";
+
+            try
+            {
+                _logger.LogDebug($@"{_className}: Getting Activity {id}");
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    result = connection.Query<Models.Activity>(sql, new { Id = id }).First();
+                }
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($@"{_className}: Error getting Activity {id} - {e.Message}");
+                return null;
+            }
+        }
+
+        public int CreateActivity(Models.Activity activity)
+        {
+            int newId;
+
+            string sql = $@"INSERT INTO dbo.Activity (AccountId, ActivityTypeId, Amount) 
+                            VALUES (@AccountId, @ActivityTypeId, @Amount)
+                            SELECT CAST(SCOPE_IDENTITY() as int)";
+
+            try
+            {
+                _logger.LogDebug($@"{_className}: Creating Activity for Account {activity.AccountId}");
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    newId = connection.Query<int>(sql, new { AccountId = activity.AccountId, ActivityTypeId = activity.ActivityTypeId, Amount = activity.Amount }).Single();
+                }
+
+                return newId;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($@"{_className}: Error creating Activity for Account {activity.AccountId} - {e.Message}");
+                return -1;
+            }
+        }
+
+        public Models.Activity UpdateActivity(Models.Activity activity)
+        {
+            string sql = $@"UPDATE dbo.Activity
+                            SET AccountId = @AccountId,
+                            ActivityTypeId = @ActivityTypeId,
+                            Amount = @Amount
+                            WHERE Id = @Id";
+
+            try
+            {
+                _logger.LogDebug($@"{_className}: Updating Activity {activity.Id} to AccountId {activity.AccountId}, ActivityTypeId {activity.ActivityTypeId}, Amount {activity.Amount}");
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    connection.Execute(sql, new { Id = activity.Id, AccountId = activity.AccountId, ActivityTypeId = activity.ActivityTypeId, Amount = activity.Amount });
+                }
+
+                return GetActivityById(activity.Id);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($@"{_className}: Error updating Activity {activity.Id} - {e.Message}");
+                return null;
+            }
+        }
+
+        public bool DeleteActivity(int activityId)
+        {
+            string sql = $@"DELETE FROM dbo.Activity
+                            WHERE Id = @Id";
+
+            try
+            {
+                _logger.LogDebug($@"{_className}: Deleting Activity {activityId}");
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    connection.Execute(sql, new { Id = activityId });
+                }
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($@"{_className}: Error deleting Activity {activityId} - {e.Message}");
+                return false;
+            }
+        }
+    }
+}
--- Rytor.Craps.Microservices.Balance/Rytor.Craps.Microservices.Balance/Repositories/BalanceRepository.cs
+++ Rytor.Craps.Microservices.Balance/Rytor.Craps.Microservices.Balance/Repositories/BalanceRepository.cs
@@ -18,149 +18,125 @@ namespace Rytor.Craps.Microservices.Balance.Repositories
         public BalanceRepository(string connectionString, ILoggerFactory loggerFactory)
         {
             _logger = loggerFactory.CreateLogger<BalanceRepository>();
-            _logger.LogInformation("AccountRepository initialized.");
+            _logger.LogInformation("BalanceRepository initialized.");
             _connectionString = connectionString;
         }
 
         public int CreateBalance(Models.Balance balance)
         {
-            throw new NotImplementedException();
+            int newId;
+
+            string sql = $@"INSERT INTO dbo.Balance (AccountId, CurrentBalance, CurrentFloor) 
+                            VALUES (@AccountId, @CurrentBalance, @CurrentFloor)
+                            SELECT CAST(SCOPE_IDENTITY() as int)";
+
+            try
+            {
+                _logger.LogDebug($@"{_className}: Creating Balance for Account {balance.AccountId}");
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    newId = connection.Query<int>(sql, new { AccountId = balance.AccountId, CurrentBalance = balance.CurrentBalance, CurrentFloor = balance.CurrentFloor }).Single();
+                }
+
+                return newId;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($@"{_className}: Error creating Balance for Account {balance.AccountId} - {e.Message}");
+                return -1;
+            }
         }
 
         public bool DeleteBalance(int accountId)
         {
-            throw new NotImplementedException();
+            string sql = $@"DELETE FROM dbo.Balance
+                            WHERE AccountId = @AccountId";
+
+            try
+            {
+                _logger.LogDebug($@"{_className}: Deleting Balance for Account {accountId}");
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    connection.Execute(sql, new { AccountId = accountId });
+                }
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($@"{_className}: Error deleting Balance for Account {accountId} - {e.Message}");
+                return false;
+            }
         }
 
         public Models.Balance GetBalanceById(int accountId)
         {
-            throw new NotImplementedException();
+            Models.Balance result;
+
+            string sql = $@"SELECT AccountId, CurrentBalance, CurrentFloor, CreateDate from dbo.Balance WHERE AccountId = @AccountId";
+
+            try
+            {
+                _logger.LogDebug($@"{_className}: Getting Balance for Account {accountId}");
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    result = connection.Query<Models.Balance>(sql, new { AccountId = accountId }).First();
+                }
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($@"{_className}: Error getting Balance for Account {accountId} - {e.Message}");
+                return null;
+            }
         }
 
         public IEnumerable<Models.Balance> GetBalances()
         {
-            throw new NotImplementedException();
+            IEnumerable<Models.Balance> result;
+
+            string sql = $@"SELECT AccountId, CurrentBalance, CurrentFloor, CreateDate from dbo.Balance";
+
+            try
+            {
+                _logger.LogDebug($@"{_className}: Getting all Balances");
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    result = connection.Query<Models.Balance>(sql);
+                }
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($@"{_className}: Error getting Balances - {e.Message}");
+                return null;
+            }
         }
 
         public Models.Balance UpdateBalance(Models.Balance balance)
         {
-            throw new NotImplementedException();
+            string sql = $@"UPDATE dbo.Balance
+                            SET CurrentBalance = @CurrentBalance,
+                            CurrentFloor = @CurrentFloor
+                            WHERE AccountId = @AccountId";
+
+            try
+            {
+                _logger.LogDebug($@"{_className}: Updating Balance for Account {balance.AccountId} to {balance.CurrentBalance}");
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    connection.Execute(sql, new { AccountId = balance.AccountId, CurrentBalance = balance.CurrentBalance, CurrentFloor = balance.CurrentFloor });
+                }
+
+                return GetBalanceById(balance.AccountId);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($@"{_className}: Error updating Balance for Account {balance.AccountId} - {e.Message}");
+                return null;
+            }
         }
-
-        // public IEnumerable<Models.Account> GetAccounts()
-        // {
-        //     IEnumerable<Models.Account> result;
-
-        //     string sql = $@"SELECT Id, TwitchId, CreateDate from dbo.Account";
-
-        //     try
-        //     {
-        //         _logger.LogDebug($@"{_className}: Getting all Accounts");
-        //         using (var connection = new SqlConnection(_connectionString))
-        //         {
-        //             result = connection.Query<Models.Account>(sql);
-        //         }
-
-        //         return result;
-        //     }
-        //     catch (Exception e)
-        //     {
-        //         _logger.LogError($@"{_className}: Error getting Accounts - {e.Message}");
-        //         return null;
-        //     }
-        // }
-
-        // public Models.Account GetAccountById(int id)
-        // {
-        //     Models.Account result;
-
-        //     string sql = $@"SELECT Id, TwitchId, CreateDate from dbo.Account WHERE Id = {id}";
-
-        //     try
-        //     {
-        //         _logger.LogDebug($@"{_className}: Getting Account {id}");
-        //         using (var connection = new SqlConnection(_connectionString))
-        //         {
-        //             result = connection.Query<Models.Account>(sql).First();
-        //         }
-
-        //         return result;
-        //     }
-        //     catch (Exception e)
-        //     {
-        //         _logger.LogError($@"{_className}: Error getting Account {id} - {e.Message}");
-        //         return null;
-        //     }
-        // }
-
-        // public int CreateAccount(Models.Account account)
-        // {
-        //     int newId;
-
-        //     string sql = $@"INSERT INTO dbo.Account (@twitchId) 
-        //                     VALUES (@TwitchId)
-        //                     SELECT CAST(SCOPE_IDENTITY() as int)";
-
-        //     try
-        //     {
-        //         _logger.LogDebug($@"{_className}: Creating Account {account.TwitchId}");
-        //         using (var connection = new SqlConnection(_connectionString))
-        //         {
-        //             newId = connection.Query<int>(sql, new { twitchId = account.TwitchId }).Single();
-        //         }
-
-        //         return newId;
-        //     }
-        //     catch (Exception e)
-        //     {
-        //         _logger.LogError($@"{_className}: Error creating Account {account.TwitchId} - {e.Message}");
-        //         return -1;
-        //     }
-        // }
-
-        // public Models.Account UpdateAccount(Models.Account account)
-        // {
-        //     string sql = $@"UPDATE dbo.Account
-        //                     SET TwitchId = @twitchId
-        //                     WHERE Id = @id";
-
-        //     try
-        //     {
-        //         _logger.LogDebug($@"{_className}: Updating Account {account.Id} to {account.TwitchId}");
-        //         using (var connection = new SqlConnection(_connectionString))
-        //         {
-        //             connection.Execute(sql, new { id = account.Id, twitchId = account.TwitchId });
-        //         }
-
-        //         return GetAccountById(account.Id);
-        //     }
-        //     catch (Exception e)
-        //     {
-        //         _logger.LogError($@"{_className}: Error updating Account {account.Id} - {e.Message}");
-        //         return null;
-        //     }
-        // }
-
-        // public bool DeleteAccount(int id)
-        // {
-        //     string sql = $@"DELETE FROM dbo.Account
-        //                     WHERE Id = @deleteId";
-
-        //     try
-        //     {
-        //         _logger.LogDebug($@"{_className}: Deleting Account {id}");
-        //         using (var connection = new SqlConnection(_connectionString))
-        //         {
-        //             connection.Execute(sql, new { deleteId = id });
-        //         }
-
-        //         return true;
-        //     }
-        //     catch (Exception e)
-        //     {
-        //         _logger.LogError($@"{_className}: Error deleting Account {id} - {e.Message}");
-        //         return false;
-        //     }
-        // }
     }
 }
